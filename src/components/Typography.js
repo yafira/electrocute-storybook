cb3d9cd00b60e0307dddb700aafe423b974e6ec7
yafira <yafira.martinez@lc.cuny.edu>
2023-08-25 14:57:47 -0400

    completed typography component and story

--- src/components/Typography.js
+++ src/components/Typography.js
@@ -1,6 +1,15 @@
 export default function Typography({ variant, label = 'text' }) {
 	let style
-	if (variant === 'h1') style = 'text-3xl font-bold'
-	if (variant === 'h2') style = 'text-2xl font-bold'
+	if (variant === 'h1') style = 'text-9xl font-mono font-regular'
+	if (variant === 'h2') style = 'text-8xl font-mono font-regular'
+	if (variant === 'h3') style = 'text-7xl font-mono font-regular'
+	if (variant === 'h4') style = 'text-6xl font-mono font-regular'
+	if (variant === 'h5') style = 'text-5xl font-mono font-regular'
+	if (variant === 'h6') style = 'text-4xl font-mono font-regular'
+	if (variant === 'body1') style = 'text-3xl font-mono font-regular'
+	if (variant === 'body2') style = 'text-2xl font-mono font-regular'
+	if (variant === 'body3') style = 'text-lg font-mono font-regular'
+	if (variant === 'body4') style = 'text-base font-mono font-regular'
+	if (variant === 'body5') style = 'text-sm font-mono font-regular'
 	return <p className={style}>{label}</p>
 }
--- src/stories/Typography.stories.js
+++ src/stories/Typography.stories.js
@@ -20,7 +20,56 @@ Headline2.args = {
 	variant: 'h2',
 }
 
-export const Body = Template.bind({})
-Body.args = {
-	label: 'body',
+export const Headline3 = Template.bind({})
+Headline3.args = {
+	label: 'Headline 3',
+	variant: 'h3',
+}
+
+export const Headline4 = Template.bind({})
+Headline4.args = {
+	label: 'Headline 4',
+	variant: 'h4',
+}
+
+export const Headline5 = Template.bind({})
+Headline5.args = {
+	label: 'Headline 5',
+	variant: 'h5',
+}
+
+export const Headline6 = Template.bind({})
+Headline6.args = {
+	label: 'Headline 6',
+	variant: 'h6',
+}
+
+export const Body1 = Template.bind({})
+Body1.args = {
+	label: 'Body 1',
+	variant: 'body1',
+}
+
+export const Body2 = Template.bind({})
+Body2.args = {
+	label: 'Body 2',
+	variant: 'body2',
+}
+
+export const Body3 = Template.bind({})
+Body3.args = {
+	label: 'Body 3',
+	variant: 'body3',
+}
+
+export const Body4 = Template.bind({})
+Body4.args = {
+	label: 'Body 4',
+	variant: 'body4',
+}
+
+export const Body5 = Template.bind({})
+Body5.args = {
+	label: 'Body 5',
+	variant: 'body5',
 }
